d8dea585d1645ee47f0a251023a1cd8a74fcb86b
JRGuchi <jamesikeguchi@gmail.com>
2020-07-12 16:28:51 -0500

    Test feature

--- FedTrack.py
+++ FedTrack.py
@@ -223,3 +223,6 @@ print("PPPLF: " + str(df_ppplf_lenders) + " PPP Lenders Backed Via " + df_ppplf_
 print("PPPLF: " + str(df_ppplf_allocation) + " of PPP Allocation ($669B) Backed")
 
 print("TOTAL: " + ('${:,.2f}'.format(df_pdcf_cost+df_cpff_cost+df_mmmflf_cost+df_talf_cost+df_smccf_cost+df_etf_value+df_mlf_value+df_ppplf_loans)) + " in Total COVID-Related Fed Balance Sheet Expansion")
+
+
+# Testing comment
